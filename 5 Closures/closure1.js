b5156f9e7c80af832dfee59f429a8b99fda816b5
Aniket <aniketraj28042003@gmail.com>
2023-12-22 11:14:25 +0530

    added asyncjs, callback and closures code

--- 5 Closures/closure1.js
+++ 5 Closures/closure1.js
@@ -10,7 +10,7 @@ console.log("Starting");
 todo("assignment");
 console.log("ending");
 /**
- * In this program Starting will be printed, then todo function will be called In which "start of todo"b will be printed.
+ * In this program Starting will be printed, then todo function will be called. In which "start of todo" will be printed.
  * At the time instance when function fun() will be called, then todo function is already over (because it is asynchronous function which will be executed after 2 sec).
  * If fun() is called after the completion of the todo, and todo has ended then how is it able to access task variable?
  * Here what happens that todo is called and is over i.e it is free from the callstack means its instance in the callstack is over.
--- 6 Async JS/async1.js
+++ 6 Async JS/async1.js
@@ -7,3 +7,11 @@ function process() {
   console.log("end");
 }
 process();
+/**
+ * In the above example, the process() will be called then inside that "start" will be printed.
+ * Then setTimeout function will be called, which will handled by the runtime.
+ * Meanwhile below that for loop will execute, assume for loop takes much more time than setTimeout() function to be exected.
+ * During that time, since the setTimeout() fucntion is executed, will be transfered to the event queue.
+ * The event loop will check everytime if the condition is true or false i.e is the callstaack is empty or not and global tasks are done or not.
+ * Once the for loop and the "end" is printed, then the callstack will be over, after that the event queue will pop the operations and the operatiosn will be shown.
+ */
