e71d8359d74719608e30c4097ce79b106d460c29
Aniket <aniketraj28042003@gmail.com>
2024-01-09 01:54:43 +0530

    added oops codes

---
+++ 9 Oops/classesIntro.js
@@ -0,0 +1,15 @@
+class Product{
+    constructor(n, p){
+        this.name = n;
+        this.price = p;
+    }
+    displayProduct(){
+
+    }
+
+    buyProduct(){
+
+    }
+};
+
+let iphone = new Product("iphone 11", 50000);
